f9f3cace83845f1839f6d753e849c1723403b03f
EvgenyIstomin <istom.evgen@gmail.com>
2026-09-11 23:36:22 +0300

    Add class Geometry

---
+++ Lesson0/src/main/java/Geometry.java
@@ -0,0 +1,12 @@
+public class Geometry {
+
+    static void main() {
+        printSquareArea(1.2);
+    }
+    static void printSquareArea(double side) {
+        System.out.println(
+                "Площадь квадрата со стороной " + side + "=" + side*side
+        );
+    }
+
+}
--- Lesson0/src/main/java/Hello.java
+++ Lesson0/src/main/java/Hello.java
@@ -3,5 +3,8 @@ public class Hello {
         System.out.println(
                 "Hello, World!"
         );
+        System.out.println(
+                "Hello" + 2*2
+        );
     }
 }
